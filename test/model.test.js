e7b4643adffa0d08273b9bed405e8709885c9bb4
Tj Holowaychuk <tj@vision-media.ca>
2010-08-21 08:14:20 -0700

    Throwing error when Model prop does not exist

--- test/model.test.js
+++ test/model.test.js
@@ -6,17 +6,27 @@
 var rapid = require('rapid'),
     Model = rapid.Model;
 
+var Movie = rapid.createModel('Movie', {
+    title: { type: 'String' },
+    desc:  { type: 'String' },
+    views: { type: 'Number' }
+});
+
 module.exports = {
     'test .createModel()': function(assert){
-        var Movie = rapid.createModel('Movie', {
-            title: { type: 'String' },
-            desc:  { type: 'String' },
-            views: { type: 'Number' }
-        });
-        
         assert.ok(new Movie instanceof Model, 'Movie does not inherit from Model');
         assert.equal('String', Movie.properties.title.type);
         assert.equal('String', Movie.properties.desc.type);
         assert.equal('Number', Movie.properties.views.type);
+    },
+    
+    'test Model()': function(assert){
+        var err;
+        try {
+            new Movie({ name: 'batman' });
+        } catch (e) {
+            err = e;
+        }
+        assert.equal(err.message, 'Movie has no property "name".');
     }
 };
