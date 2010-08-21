72e9934576ab880c3f59f9674ddcff0fdeb4131f
Tj Holowaychuk <tj@vision-media.ca>
2010-08-21 08:08:03 -0700

    More Model tests

--- test/model.test.js
+++ test/model.test.js
@@ -10,10 +10,13 @@ module.exports = {
     'test .createModel()': function(assert){
         var Movie = rapid.createModel('Movie', {
             title: { type: 'String' },
+            desc:  { type: 'String' },
             views: { type: 'Number' }
         });
         
         assert.ok(new Movie instanceof Model, 'Movie does not inherit from Model');
         assert.equal('String', Movie.properties.title.type);
+        assert.equal('String', Movie.properties.desc.type);
+        assert.equal('Number', Movie.properties.views.type);
     }
 };
