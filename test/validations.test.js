f5da53a2c603c2b768270ebeb86e45bca3186246
Tj Holowaychuk <tj@vision-media.ca>
2010-08-24 16:00:12 -0700

    Added min validation

--- test/validations.test.js
+++ test/validations.test.js
@@ -7,7 +7,8 @@ var rapid = require('rapid'),
     validation = rapid.validations;
 
 var User = rapid.createModel('User', {
-    name: { type: 'String', required: true }
+    name: { type: 'String', required: true },
+    age: { type: 'Number', min: 1, max: 120 }
 });
 
 module.exports = {
@@ -18,6 +19,19 @@ module.exports = {
             assert.equal('User "name" is required', err.message);
             user.name = 'tj';
             validation.required(user, prop, function(err){
+                assert.isUndefined(err);
+                done();
+            });
+        });
+    },
+    
+    'test min': function(assert, done){
+        var user = new User({ name: 'tj', age: 0 }),
+            prop = user.properties.get('age');
+        validation.min(user, prop, function(err){
+            assert.equal('User "age" of 0 is below the minimum of 1', err.message);
+            user.age = 23;
+            validation.min(user, prop, function(err){
                 assert.isUndefined(err);
                 done();
             });
