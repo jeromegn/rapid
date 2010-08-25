4fc3b1e21d33341dc173a0e411ddfa63d42a2000
Tj Holowaychuk <tj@vision-media.ca>
2010-08-24 17:39:48 -0700

    Refactor tests

--- test/validations.test.js
+++ test/validations.test.js
@@ -21,93 +21,66 @@ function test(validationName, record, prop, errMsg, okVal, fn) {
         record[prop.name] = okVal;
         validation[validationName](record, prop, function(err){
             assert.isUndefined(err);
-            fn();
+            fn && fn();
         });
     });
 }
 
 module.exports = {
-    'test required': function(assert, done){
+    'test required': function(assert){
         test('required', 
             new User, 
             'name', 
             'User name is required', 
-            'tyler', 
-            done);
+            'tyler');
     },
     
-    'test min': function(assert, done){
+    'test min with number': function(assert){
         test('min', 
             new User({ name: 'tyler', age: 0 }),
             'age',
             'User age 0 is below the minimum of 1',
-            23,
-            done);
-        
+            23);
+    },
+    
+    'test min with string': function(assert){
         test('min',
             new User({ name: 'tj' }),
             'name',
             "User name 'tj' is below the minimum length of 3",
-            'tyler',
-            done);
+            'tyler');
     },
     
-    'test max': function(assert, done){
+    'test max with number': function(assert){
         test('max', 
             new User({ name: 'tyler', age: 250 }),
             'age',
             'User age 250 is above the maximum of 120',
-            30,
-            done);
-
+            30);
+    },
+    
+    'test max with string': function(assert){
         var largeName = Array(20).join('fail');
         test('max', 
             new User({ name: largeName }),
             'name',
             'User name \'' + largeName + '\' is above the maximum length of 32',
-            'something smaller',
-            done);
+            'something smaller');
     },
     
-    'test max': function(assert, done){
-        test('max', 
-            new User({ name: 'tyler', age: 250 }),
-            'age',
-            'User age 250 is above the maximum of 120',
-            30,
-            done);
-
-        var largeName = Array(20).join('fail');
-        test('max', 
-            new User({ name: largeName }),
-            'name',
-            'User name \'' + largeName + '\' is above the maximum length of 32',
-            'something smaller',
-            done);
-    },
-    
-    'test format': function(assert, done){
+    'test format': function(assert){
         test('format', 
             new User({ name: 'tyler%4' }),
             'name',
             'User name format is invalid',
-            'tyler',
-            done);
+            'tyler');
     },
     
-    'test "postal-code" format': function(assert, done){
+    'test "postal-code" format': function(assert){
         test('format', 
             new User({ name: 'tyler', postalCode: 'asdf' }),
             'postalCode',
             'User postalCode format is invalid',
-            'v9b1t7',
-            done);
-
-        test('format', 
-            new User({ name: 'tyler', postalCode: '123' }),
-            'postalCode',
-            'User postalCode format is invalid',
-            'V9B1T7',
-            done);
+            'v9b1t7');
     }
 };
