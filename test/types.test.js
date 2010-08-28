81ed3754cbd9d4457b1483d745cd1240137c12bc
Tj Holowaychuk <tj@vision-media.ca>
2010-08-28 00:38:14 -0700

    Added "date" type

--- test/types.test.js
+++ test/types.test.js
@@ -8,9 +8,21 @@ var rapid = require('rapid'),
 
 module.exports = {
     'test "string"': function(assert){
-        assert.equal('test', types.string.save('test'), 'test "string" string support ;)');
+        assert.equal('test', types.string.save('test'), 'test "string" string support');
         assert.equal('test', types.string.save(new Buffer('test')), 'test "string" Buffer support');
         assert.equal('15', types.string.save(15), 'test "string" Number support');
         assert.throws(function(){ types.string.save(new Date); });
+        assert.throws(function(){ types.string.save(); });
+    },
+
+    'test "date"': function(assert){
+        var date = new Date('may 25 1987'),
+            timestamp = Number(date).toString();
+        assert.equal(timestamp, types.date.save(date), 'test "date" Date support');
+        assert.throws(function(){ types.date.save('fail'); });
+        assert.throws(function(){ types.date.save(123123); });
+        assert.throws(function(){ types.date.save(); });
+        
+        assert.equal(date.toString(), types.date.load(timestamp));
     }
 };
