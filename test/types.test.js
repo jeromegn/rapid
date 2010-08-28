e7ed7a88dddbfab31897dcfd51687eed11b991ac
Tj Holowaychuk <tj@vision-media.ca>
2010-08-28 00:44:59 -0700

    save -> dump

--- test/types.test.js
+++ test/types.test.js
@@ -8,20 +8,20 @@ var rapid = require('rapid'),
 
 module.exports = {
     'test "string"': function(assert){
-        assert.equal('test', types.string.save('test'), 'test "string" string support');
-        assert.equal('test', types.string.save(new Buffer('test')), 'test "string" Buffer support');
-        assert.equal('15', types.string.save(15), 'test "string" Number support');
-        assert.throws(function(){ types.string.save(new Date); });
-        assert.throws(function(){ types.string.save(); });
+        assert.equal('test', types.string.dump('test'), 'test "string" string support');
+        assert.equal('test', types.string.dump(new Buffer('test')), 'test "string" Buffer support');
+        assert.equal('15', types.string.dump(15), 'test "string" Number support');
+        assert.throws(function(){ types.string.dump(new Date); });
+        assert.throws(function(){ types.string.dump(); });
     },
 
     'test "date"': function(assert){
         var date = new Date('may 25 1987'),
             timestamp = Number(date).toString();
-        assert.equal(timestamp, types.date.save(date), 'test "date" Date support');
-        assert.throws(function(){ types.date.save('fail'); });
-        assert.throws(function(){ types.date.save(123123); });
-        assert.throws(function(){ types.date.save(); });
+        assert.equal(timestamp, types.date.dump(date), 'test "date" Date support');
+        assert.throws(function(){ types.date.dump('fail'); });
+        assert.throws(function(){ types.date.dump(123123); });
+        assert.throws(function(){ types.date.dump(); });
 
         assert.equal(date.toString(), types.date.load(timestamp));
         assert.throws(function(){ types.date.load('fail'); });
