a9a489c81f03c90ef4c5a3033dd5ec52e6a5cc8f
Tj Holowaychuk <tj@vision-media.ca>
2010-08-28 02:58:12 -0700

    Started examples

---
+++ examples/basic.js
@@ -0,0 +1,34 @@
+
+/**
+ * Module dependencies.
+ */
+
+var rapid = require('../index');
+
+var User = rapid.createModel('User', {
+    first  : { type: 'string', required: true },
+    last   : { type: 'string', required: true },
+    email  : { type: 'string', format: 'email' },
+    age    : { type: 'number', min: 1, max: 120 },
+    image  : { type: 'binary' } 
+});
+
+var tj = new User({
+    first: 'TJ',
+    last: 'Holowaychuk',
+    age: 23,
+    email: 'invalid'
+});
+
+// TODO: fix callback which is called twice...
+
+tj.save(function(err){
+    // access err.record and err.property
+    console.log('error: %s', err.message);
+
+    // Fix and re-save
+    tj.email = 'tj@vision-media.ca';
+    tj.save(function(err){
+        console.log('test');
+    });
+});
