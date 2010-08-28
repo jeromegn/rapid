f9ae9ddf033ed4ac00ee5cf985cde5234e6b3d91
Tj Holowaychuk <tj@vision-media.ca>
2010-08-28 10:57:39 -0700

    Types always dump Buffer instances

--- test/types.test.js
+++ test/types.test.js
@@ -14,12 +14,14 @@ module.exports = {
         assert.throws(function(){ types.id.coerce(123); });
         
         assert.equal(id, types.id.load(new Buffer(id)));
+        assert.eql(new Buffer(id), types.id.dump(id));
     },
 
     'test "string"': function(assert){
         assert.eql('test', types.string.coerce('test'), 'test "string" string support');
         assert.eql('test', types.string.coerce(new Buffer('test')), 'test "string" Buffer support');
         assert.eql('15', types.string.coerce(15), 'test "string" Number support');
+        assert.eql(new Buffer('test'), types.string.dump('test'));
         assert.throws(function(){ types.string.coerce(new Date); });
         assert.throws(function(){ types.string.coerce(); });
     },
@@ -32,7 +34,7 @@ module.exports = {
         assert.throws(function(){ types.date.coerce(123123); });
         assert.throws(function(){ types.date.coerce(); });
 
-        assert.equal(timestamp, types.date.dump(date));
+        assert.eql(new Buffer(timestamp), types.date.dump(date));
 
         assert.equal(date.toString(), types.date.load(timestamp));
         assert.throws(function(){ types.date.load('fail'); });
@@ -42,10 +44,10 @@ module.exports = {
     'test "json"': function(assert){
         assert.eql({ foo: 'bar' }, types.json.coerce({ foo: 'bar' }));
         
-        assert.eql('{"foo":"bar"}', types.json.dump({ foo: 'bar' }));
-        assert.eql('123', types.json.dump(123));
-        assert.eql('"test"', types.json.dump("test"));
-        assert.eql('""', types.json.dump(""));
+        assert.eql(new Buffer('{"foo":"bar"}'), types.json.dump({ foo: 'bar' }));
+        assert.eql(new Buffer('123'), types.json.dump(123));
+        assert.eql(new Buffer('"test"'), types.json.dump("test"));
+        assert.eql(new Buffer('""'), types.json.dump(""));
         
         assert.eql({ foo: 'bar' }, types.json.load('{"foo":"bar"}'));
         assert.strictEqual(123, types.json.load('123'));
@@ -59,7 +61,7 @@ module.exports = {
         assert.eql(15.99, types.number.coerce('15.99'));
         assert.throws(function(){ types.number.coerce('fail'); });
         
-        assert.equal('123', types.number.dump(123));
+        assert.eql(new Buffer('123'), types.number.dump(123));
         
         assert.strictEqual(123, types.number.load(new Buffer('123')));
         assert.strictEqual(123, types.number.load('123'));
@@ -70,6 +72,7 @@ module.exports = {
     'test "binary"': function(assert){
         assert.eql(new Buffer('test'), types.binary.coerce(new Buffer('test')));
         assert.eql(new Buffer('test'), types.binary.load(new Buffer('test')));
+        assert.eql(new Buffer('test'), types.binary.dump(new Buffer('test')));
     },
     
     'test "boolean"': function(assert){
@@ -89,8 +92,8 @@ module.exports = {
         assert.eql(true, types.boolean.coerce(-1));
         assert.eql(true, types.boolean.coerce(-2));
         
-        assert.equal('1', types.boolean.dump(true));
-        assert.equal('0', types.boolean.dump(false));
+        assert.eql(new Buffer('1'), types.boolean.dump(true));
+        assert.eql(new Buffer('0'), types.boolean.dump(false));
         
         assert.strictEqual(true, types.boolean.load(new Buffer('1')));
         assert.strictEqual(false, types.boolean.load(new Buffer('0')));
