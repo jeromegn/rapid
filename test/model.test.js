333af1bf4721f73076d7f7774a0dfa2e0f12895c
Tj Holowaychuk <tj@vision-media.ca>
2010-08-31 15:00:58 -0700

    Added test for "gt"

--- test/model.test.js
+++ test/model.test.js
@@ -296,6 +296,20 @@ module.exports = {
         });
     },
     
+    'test Model.find() gt': function(assert, done){
+        var a = new Movie({ title: 'foo', sales: 1 }),
+            b = new Movie({ title: 'bar', sales: 1 }),
+            c = new Movie({ title: 'baz', sales: 3 });
+        new Collection([a,b,c]).save(function(){
+            Movie.find({ sales: { gt: 1 }}).all(function(err, movies){
+                assert.ok(!err);
+                assert.length(movies, 1);
+                assert.equal('baz', movies[0].title);
+                done();
+            });
+        });
+    },
+    
     'test defaults': function(assert, done){
         var movie = new Movie({ title: 'something' });
         movie.save(function(err){
